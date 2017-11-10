90152c81578636ed963f12ec5ef66b7473bfdf47
Simon Buechi <simon.buechi@gmail.com>
2017-11-10 17:42:53 +0100

    shortcut fixes

--- modules/rp_shortcut/js/rp_shortcut.js
+++ modules/rp_shortcut/js/rp_shortcut.js
@@ -10,14 +10,11 @@
 
   Drupal.behaviors.rp_shortcut = {
     attach: function () {
-
-    Mousetrap.bind('4', function() { console.log('keystroke 4'); });
-
-    Mousetrap.bind('i', function() { window.location = Drupal.url('index'); });
-    Mousetrap.bind('b', function() { window.location = Drupal.url('bulk'); });
-    Mousetrap.bind('s', function() { window.location = Drupal.url('s'); });
-    Mousetrap.bind('h', function() { window.location = Drupal.url('home'); });
-
+//    Mousetrap.bind('4', function() { console.log('keystroke 4'); });
+    Mousetrap.bind('shift+i', function() { window.location = Drupal.url('index'); });
+    Mousetrap.bind('shift+b', function() { window.location = Drupal.url('bulk'); });
+    Mousetrap.bind('shift+s', function() { window.location = Drupal.url('s'); });
+    Mousetrap.bind('shift+h', function() { window.location = Drupal.url('home'); });
     Mousetrap.bind('shift+k', function() { window.location = Drupal.url('node/add/knowledge'); });
     Mousetrap.bind('shift+u', function() { window.location = Drupal.url('node/add/publication'); });
     Mousetrap.bind('shift+p', function() { window.location = Drupal.url('node/add/person'); });
@@ -25,7 +22,6 @@
     Mousetrap.bind('shift+l', function() { window.location = Drupal.url('node/add/place'); });               
     Mousetrap.bind('shift+o', function() { window.location = Drupal.url('node/add/object'); });
     Mousetrap.bind('shift+c', function() { window.location = Drupal.url('node/add/collection'); });
-
 /**
     Mousetrap.bind('shift+s', function() {  });   // save form
     Mousetrap.bind('shift+left', function() {  });   // previous item   
